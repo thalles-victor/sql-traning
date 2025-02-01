050e9d836a57ee3cd88ce4453b406457f9937c0d
thalles-victor <thalles.victor2012@gmail.com>
2025-02-01 20:20:36 -0300

    :sparkles: feat: implement search builder from brand.

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def get_cars():
     )
 
 @app.route("/brand", methods=["POST"])
-def create_car():
+def create_brand():
     body = request.json
 
     brandEntity = {
@@ -38,5 +38,30 @@ def create_car():
         jsonify(message="Brand created", data=created_brand)
     )
 
+
+@app.route("/brands", methods=["GET"])
+def find_brands():
+    page = int(request.args.get("page", 1))
+    limit = int(request.args.get("limit", 10))
+
+    filters = {}
+    if "name" in request.args:
+        filters["name"] = request.args["name"]
+    if "description" in request.args:
+        filters["description"] = request.args["description"]
+    
+    result = brandRepository.find_many(page=page, limit=limit, filters=filters)
+    
+    print(result)
+    meta = {
+        "total": result["total"],
+        "page": result["page"],
+        "limit": result["limit"],
+        "total_pages": result["total_pages"]
+    }
+
+    print(result["data"])
+
+    return  make_response(jsonify(message="Brands limit", data = result["data"], meta=meta))
 
 app.run()
--- repositories/BrandRepository.py
+++ repositories/BrandRepository.py
@@ -1,4 +1,5 @@
 import psycopg2
+from psycopg2 import sql
 from database.connection import connection_config
 
 class BrandRepository:
@@ -16,3 +17,66 @@ class BrandRepository:
     conn.close()
 
     return result
+  
+  def find_many(self, page=1, limit=10, filters=None):
+    conn = psycopg2.connect(**connection_config)
+    cursor = conn.cursor()
+
+    try:
+      page = max(1, int(page))
+      limit = min(10, max(1, int(limit)))
+
+
+      query = sql.SQL('SELECT * FROM "brand"')
+      params = []
+
+      if filters:
+        filter_clauses = []
+        for key, value in filters.items():
+          if key not in ["name", "description"]:
+            continue
+
+          filter_clauses.append(sql.SQL("{} ILIKE %s").format(sql.Identifier(key)))
+          params.append(f"%{value}%")
+        query += sql.SQL(" WHERE ") + sql.SQL(" AND ").join(filter_clauses)
+      
+      # pagination
+      offset = (page - 1) * limit
+      query += sql.SQL(" ORDER BY created_at DESC LIMIT %s OFFSET %s")
+      params.extend([limit, offset])
+
+      cursor.execute(query, tuple(params))
+      results = cursor.fetchall()
+
+      # Obtém os nomes das colunas
+      colunas = [desc[0] for desc in cursor.description]
+
+      # Converte a lista de tuplas para uma lista de dicionários
+      data = [dict(zip(colunas, row)) for row in results]
+
+
+      # count all registers without pagination
+      count_query = sql.SQL('SELECT COUNT(*) FROM "brand"')
+      if filters:
+        count_query += sql.SQL(" WHERE ") + sql.SQL(" AND ").join(filter_clauses)
+      cursor.execute(count_query, tuple(params[:-2]))
+      total_items = cursor.fetchone()[0]
+      
+      cursor.close()
+      conn.close()
+
+      print(results)
+      
+      return {
+        "data": data,
+        "total": total_items,
+        "page": page,
+        "limit": limit,
+        "total_pages": (total_items + limit -1) // limit
+      }
+    except Exception as e:
+      print("Error:", e)
+      return {"errors": "Database error"}
+    finally:
+      cursor.close()
+      conn.close()
